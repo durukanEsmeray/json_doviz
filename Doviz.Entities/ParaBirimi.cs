c84ce719c801b3cb051f1f15461368058091f234
durukanEsmeray <durukanesmeray@gmail.com>
2023-03-22 13:25:19 +0300

    Entities katmanı yapıldı, sql tablolarına karşılık gelen class lar oluşturuldu.

---
+++ Doviz.Entities/ParaBirimi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Doviz.Entities
+{
+    public class ParaBirimi
+    {
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+        public string Tanim { get; set; }
+        public decimal UyariLimit { get; set; }
+
+    }
+}
